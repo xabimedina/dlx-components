4c0b93f535091bfe1e42f4163c5ae35827ae9fef
Juan Murga <juanmurga97@gmail.com>
2025-06-07 09:57:40 +0200

    First commit

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './button';
+export * from './custom/dlx-paragraph';
+export * from './aspect-ratio';
+export * from './card';
+export * from './hover-card';
+export * from './scroll-area';
+
